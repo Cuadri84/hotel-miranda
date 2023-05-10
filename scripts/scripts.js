8b0a78eebc85881bfb79b63451300cf35df59b52
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-05-10 17:56:10 +0200

    done the header scroll logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,3 +7,23 @@ burgerMenu.addEventListener("click", () => {
   navBar.classList.toggle("active");
   burgerMenu.classList.toggle("animation");
 });
+
+//header hiden when scrolled
+let initialScroll = window.scrollY;
+
+window.addEventListener("scroll", function () {
+  let newScroll = window.scrollY;
+  let width = window.innerWidth;
+
+  if (width > 1000 && initialScroll >= newScroll) {
+    document.getElementById("header").style.top = "40px";
+  } else if (width > 1000 && initialScroll <= newScroll) {
+    document.getElementById("header").style.top = "-120px";
+  }
+
+  if (width < 1000) {
+    document.getElementById("header").style.top = "0px";
+  }
+
+  initialScroll = newScroll;
+});
